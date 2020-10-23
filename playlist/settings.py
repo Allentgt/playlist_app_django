b320a6d02c2761aee9ce29dff7a47c1cf4db8e16
sabya93 <sabya93@gmail.com>
2020-10-23 15:22:44 +0530

    support email API

--- playlist/settings.py
+++ playlist/settings.py
@@ -134,5 +134,9 @@ else:
     STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'info.playlistapp@gmail.com'
+EMAIL_HOST_PASSWORD = 'JaiMataDi'
