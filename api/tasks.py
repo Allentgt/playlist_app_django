458b6319164f7ae541629d3dada541f1a8f97369
sabya93 <sabya93@gmail.com>
2020-10-31 19:29:59 +0530

    async execution implementation

---
+++ api/tasks.py
@@ -0,0 +1,15 @@
+import time
+from pytube import YouTube
+from celery.decorators import task
+
+
+@task(name='sleep_for_10')
+def sleep_for_10():
+    time.sleep(10)
+
+
+@task(name='download_and_save_music_locally')
+def download_and_save_music_locally(name, link):
+    yt = YouTube(link)
+    stream = yt.streams.filter(only_audio=True).first()
+    stream.download(f'music/')
